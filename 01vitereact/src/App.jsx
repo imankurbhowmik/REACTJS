871994051595674482dec3eb49623f63a5d54a35
Ankur Bhowmik <bhowmikankur07112002@gmail.com>
2024-04-03 15:29:34 +0000

    react new commit

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,10 +2,11 @@ import Chai from "./Chai"
 
 function App() {
   
-
+  const usrname = "Ankur"
   return (
     <>
       <Chai/>
+      <h1>my name is {usrname}</h1>
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+const username = "Ankur"
+
+const ReactElement = React.createElement(
+    'a', 
+    {href: "https://www.google.com", target: "_blank"},
+    "click me to visit google",
+    username
+)
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <App />
+    
+    
+    // ReactElement
+
+    <App/>
+    
+    
+    
   
 )
