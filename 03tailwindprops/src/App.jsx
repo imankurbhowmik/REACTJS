c6c7a55ae1ff73464610157cb451a81c5532bd7c
Ankur Bhowmik <bhowmikankur07112002@gmail.com>
2024-04-10 14:56:14 +0000

    new react commit

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,14 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  let myObj = {
+    username  : "Ankur",
+    age : 21
+  }
+
   return (
     <>
-      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
+    <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      <Card username = "Ankur" someObj = {myObj} btnText = "click me" />
+      <Card username = "Hitesh" btnText = "visit me"/>
     </>
+    
+
   )
 }
 
