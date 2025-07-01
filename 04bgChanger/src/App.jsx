8b2adc789924f2bd8e15e88d017d15432e5ac9bd
Ankur Bhowmik <bhowmikankur07112002@gmail.com>
2025-07-01 04:57:37 +0000

    add new buttons

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -14,6 +14,22 @@ function App() {
             onClick={() => {setColor("green")}}>green</button>
             <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor : "blue"}}
             onClick={() => {setColor("blue")}}>blue</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor : "violet"}}
+            onClick={() => {setColor("violet")}}>violet</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor : "skyblue"}}
+            onClick={() => {setColor("skyblue")}}>skyblue</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor : "magenta"}}
+            onClick={() => {setColor("magenta")}}>magenta</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor : "black"}}
+            onClick={() => {setColor("black")}}>black</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor : "yellow"}}
+            onClick={() => {setColor("yellow")}}>yellow</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor : "pink"}}
+            onClick={() => {setColor("pink")}}>pink</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor : "silver"}}
+            onClick={() => {setColor("silver")}}>silver</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor : "brown"}}
+            onClick={() => {setColor("brown")}}>brown</button>
           </div>
         </div>
       </div>
